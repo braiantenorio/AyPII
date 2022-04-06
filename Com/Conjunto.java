3400f0579bc16a37080e16800f4f0ffeda812cad
Braian Tenorio <tenoriovidalbraian@gmail.com>
2022-04-06 10:38:39 -0300

    xd

---
+++ Com/Conjunto.java
@@ -0,0 +1,51 @@
+package Com;
+
+import java.util.Arrays;
+
+public class Conjunto {
+	private int j=100;
+	private boolean a[];
+	
+	public Conjunto() {
+		a = new boolean[j];
+		Arrays.fill(this.a, Boolean.FALSE);
+	}
+	
+	
+	
+
+	public boolean[] getA() {
+		return a;
+	}
+
+	public void setA(boolean[] a) {
+		this.a = a;
+	}
+	
+	public void insertarElemento(int n) {
+		this.a[n]=true;
+		
+	}
+	
+	public void eliminarElemento(int n) {
+		this.a[n]=false;
+	}
+	
+	public void aStingConjunto() {
+		//int[] f = new int [j];
+		for(int i=0; i<j; i++) {
+			if (this.a[i]== true) {
+				System.out.print(i+ " ");//f[i]= i;
+			}
+		}
+		//System.out.println(Arrays.toString(f));
+	}
+	
+	/*public void esIgualA(Conjunto b) {
+		//b[1]=5;
+		
+		boolean k = Arrays.deepEquals(b, this.a);
+		System.out.println(k);
+		
+	}*/
+}
---
+++ Emd/Circulo.java
@@ -0,0 +1,18 @@
+package Emd;
+
+
+public class Circulo {
+
+	public static double diametro(double radio) {
+		return radio * 2;
+	}
+
+	public static double circunferencia(double radio) {
+		return 2 * Math.PI * radio;
+	}
+
+	public static double area(double radio) {
+		return Math.PI * Math.pow(radio, 2);
+	}
+}
+
