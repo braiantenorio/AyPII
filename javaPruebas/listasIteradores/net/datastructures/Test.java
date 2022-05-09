ff8dc030ffa33dedac5b241b94affcf4a2f24375
Braian Tenorio <tenoriovidalbraian@gmail.com>
2022-05-09 18:50:08 -0300

    asdfads

--- javaPruebas/listasIteradores/net/datastructures/Test.java
+++ javaPruebas/listasIteradores/net/datastructures/Test.java
@@ -1,4 +1,6 @@
-import net.datastructures.LinkedPositionalList;
+package net.datastructures;
+
+import java.util.Iterator;
 
 public class Test {
     private static LinkedPositionalList<String> lista;
@@ -6,12 +8,28 @@ public class Test {
     
     public static void main(String[] args) {
         lista= new LinkedPositionalList<String>();
+        Position<String> result;
+        Position<String> j;
+        Position<String> h;
+        j=lista.addFirst("leon");
+        lista.addLast("lucas");
+        result =lista.first();
+        h=lista.addAfter(result,"braian");
+        lista.addAfter(j, "Mario");
+        lista.addAfter(h, "Marito");
+        
+        //System.out.println(lista);
 
-        lista.addFirst("leon");
-        x=lista.first();
+        Iterator<String> iter= lista.iterator();
+        while(iter.hasNext()) {
+            String i= iter.next();
+            System.out.println(i);
         
-        
-        
-        
+
+        }
+
     }
+        
 }
+
+
